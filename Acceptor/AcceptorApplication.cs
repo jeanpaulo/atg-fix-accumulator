0ebcf2654f63352e587301e17a5dd5f1ad501311
Jean Paulo Amaral <jeanpaulo0@gmail.com>
2024-06-10 21:30:56 -0300

    Implementa um Acceptor simples

---
+++ Acceptor/AcceptorApplication.cs
@@ -0,0 +1,74 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acceptor;
+public class AcceptorApplication : QuickFix.MessageCracker, QuickFix.IApplication
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    int orderID = 0;
+    int execID = 0;
+
+    #region QuickFix.Application Methods
+    public void FromApp(Message message, SessionID sessionID)
+    {
+        Console.WriteLine("IN:  " + message);
+        Crack(message, sessionID);
+    }
+
+    public void ToApp(Message message, SessionID sessionID)
+    {
+        Console.WriteLine("OUT: " + message);
+    }
+
+    public void FromAdmin(Message message, SessionID sessionID) { }
+    public void OnCreate(SessionID sessionID) { }
+    public void OnLogout(SessionID sessionID) { }
+    public void OnLogon(SessionID sessionID) { }
+    public void ToAdmin(Message message, SessionID sessionID) { }
+    #endregion
+
+
+    public async void OnMessage(QuickFix.FIX44.NewOrderSingle n, SessionID s)
+    {
+        Console.WriteLine("\nMensagem chegando...");
+
+        try
+        {
+            Symbol symbol = n.Symbol;
+            Side side = n.Side;
+            OrdType ordType = n.OrdType;
+            OrderQty orderQty = n.OrderQty;
+            Price price = new Price(n.Price.Obj);
+            ClOrdID clOrdID = n.ClOrdID;
+
+            Console.WriteLine($"Simbolo: {symbol.Obj}");
+            Console.WriteLine($"Lado: {side.Obj}");
+            Console.WriteLine($"Quantidade: {orderQty.Obj}");
+            Console.WriteLine($"Preço: {price.Obj}");
+
+            QuickFix.FIX44.ExecutionReport exReport = new QuickFix.FIX44.ExecutionReport();
+
+            exReport.Set(clOrdID);
+            exReport.Set(symbol);
+            exReport.Set(orderQty);
+            exReport.Set(new LastQty(orderQty.getValue()));
+            exReport.Set(new LastPx(price.getValue()));
+
+            Session.SendToTarget(exReport, s);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error on AcceptorApplication/OnMessage: {ex.Message}");
+            throw;
+        }
+    }
+
+
+}
--- Acceptor/Program.cs
+++ Acceptor/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Acceptor;
+using QuickFix;
+using QuickFix.Fields;
+
+class Program
+{
+    private const string HttpServerPrefix = "http://127.0.0.1:5080/";
+
+    static void Main(string[] args)
+    {
+
+		try
+		{
+			var configFile = new StreamReader("./server.cfg");
+
+
+            SessionSettings settings = new SessionSettings(configFile);
+            IApplication executorApp = new AcceptorApplication();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(executorApp, storeFactory, settings, logFactory);
+            HttpServer srv = new HttpServer(HttpServerPrefix, settings);
+
+            acceptor.Start();
+            srv.Start();
+
+            Console.WriteLine(HttpServerPrefix);
+            Console.WriteLine("Aperter <enter> para sair");
+            Console.Read();
+
+            srv.Stop();
+            acceptor.Stop();
+        }
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+
+
+    }
+}
